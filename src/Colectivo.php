d0ac1e5c6418a6f04d646292b2e060dd11815b86
martualarc <martina.alarcon029@gmail.com>
2023-08-22 16:57:45 -0300

    Creada la clase tarjeta
    Las opciones de carga no estan definidas

--- src/Colectivo.php
+++ src/Colectivo.php
@@ -1,7 +1,30 @@
 <?php
 namespace TrabajoSube;
+
+class Tarjeta{
+    $saldo;
+    $limite;
+    
+    function __construct(){
+        $this->saldo = 0;
+        $this->limite = 6600; 
+    }
+
+    function cargar($monto){
+        if($this->saldo + $monto <= $this->limite){
+            $this->saldo = $this->saldo + $monto;
+        }
+        else{
+            echo "La carga excede el limite";
+        }
+    }
+
+}
+
+
+
 class Colectivo{
-    functon pagarCon($tarjeta){
+    function pagarCon($tarjeta){
         
     }
 }
