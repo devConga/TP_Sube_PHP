3e8fac9f5340a2c384d895401da6018783b02b58
martualarc <martina.alarcon029@gmail.com>
2023-08-22 17:09:26 -0300

    Creada la clase coletivo

--- src/Colectivo.php
+++ src/Colectivo.php
@@ -21,10 +21,25 @@ class Tarjeta{
 
 }
 
+class Boleto{
+    
+}
 
 
 class Colectivo{
+
+    $boletoNormal;
+
+    function __construct(){
+        $this->boletoNormal = 120;
+    }
+
     function pagarCon($tarjeta){
-        
+        if($tarjeta->saldo < $this->boletoNormal){
+            echo 'Saldo insuficiente';
+        }
+        else{
+            $tarjeta->saldo = $tarjeta->saldo - $this->boletoNormal;
+        }
     }
 }
