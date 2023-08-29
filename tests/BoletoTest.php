07296883a0eec09cef7c1291a6e97a6da7ed5681
devConga <alejandro.combasanz2004@gmail.com>
2023-08-29 17:11:34 -0300

    Añadido BoletoTest

---
+++ tests/BoletoTest.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace TrabajoSube;
+
+use PHPUnit\Framework\TestCase;
+
+class BoletoTest extends TestCase{
+   
+    public function testEsBoleto(){
+    $colectivo = new Colectivo();
+    $tarjeta = new Tarjeta();
+    $tarjeta->cargar(300);
+    $this->assertInstanceOf("Boleto", $colectivo->pagarCon($tarjeta));
+}
+
+}
