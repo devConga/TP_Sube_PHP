ddfacbfe7e824fc6a99947594c4877c69d3e5df4
devConga <alejandro.combasanz2004@gmail.com>
2023-08-29 16:53:15 -0300

    Test adentro de funcion

--- tests/ColectivoTest.php
+++ tests/ColectivoTest.php
@@ -6,9 +6,10 @@ use PHPUnit\Framework\TestCase;
 
 class ColectivoTest extends TestCase{
    
+    public function testPagarCon(){
     $colectivo = new Colectivo();
     $tarjeta = new Tarjeta();
     
-    $this->assertFalse($colectivo->pagarCon($tarjeta));
+    $this->assertFalse($colectivo->pagarCon($tarjeta));}
 
 }
--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -6,12 +6,13 @@ use PHPUnit\Framework\TestCase;
 
 class TarjetaTest extends TestCase{
    
+    public function testCargar(){
     $tarjeta = new Tarjeta();
 
     $this->assertTrue($tarjeta->cargar(250));
     $this->assertTrue($tarjeta->cargar(600));
     $this->assertTrue($tarjeta->cargar(2000));
     $this->assertFalse($tarjeta->cargar(270));
-    $this->assertFalse($tarjeta->cargar(100));
+    $this->assertFalse($tarjeta->cargar(100));}
 
 }
