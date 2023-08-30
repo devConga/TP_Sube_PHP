1db0b837c6b143f070118f3660953309dc5b8209
Conga <alejandro.combasanz2004@gmail.com>
2023-08-30 01:03:22 -0300

    Hecho el test de la franquicia completa

--- tests/ColectivoTest.php
+++ tests/ColectivoTest.php
@@ -9,6 +9,7 @@ class ColectivoTest extends TestCase{
     public function testPagarCon(){
     $colectivo = new Colectivo();
     $tarjeta = new Tarjeta();
+    $tarjeta->saldo = -200;
     
     $this->assertFalse($colectivo->pagarCon($tarjeta));}
 
---
+++ tests/TFranquiciaCompletaTest.php
@@ -0,0 +1,19 @@
+<?php 
+
+namespace TrabajoSube;
+
+use PHPUnit\Framework\TestCase;
+
+class TFranquiciaCompletaTest extends TestCase{
+
+    public function siemprePuedePagar(){
+    
+        $tarjeta = new TFranquiciaCompleta();
+        $colectivo = new Colectivo();
+        
+        $tarjeta->saldo = -211.84;
+        this->assertTrue($colectivo->pagarCon($tarjeta));
+
+    }
+
+}
