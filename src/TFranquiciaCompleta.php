337227573ad2e314091d1594b0aadd16beacfeaa
Conga <alejandro.combasanz2004@gmail.com>
2023-08-30 00:11:25 -0300

    Cambiado el constructor de las franquicias

--- src/TFranquiciaCompleta.php
+++ src/TFranquiciaCompleta.php
@@ -6,6 +6,7 @@ class TarjetaFranquiciaCompleta extends Tarjeta{
     public $porcentajeDescuento;
 
     function __construct(){
+        parent::__construct();
         $this->porcentajeDescuento = 50;
     }
 }
--- src/TFranquiciaParcial.php
+++ src/TFranquiciaParcial.php
@@ -6,6 +6,7 @@ class TarjetaFranquiciaParcial extends Tarjeta{
     public $porcentajeDescuento;
 
     function __construct(){
+        parent::__construct();
         $this->porcentajeDescuento = 100;
     }
 }
