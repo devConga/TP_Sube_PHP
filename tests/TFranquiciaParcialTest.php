5b994fb09266c67bda1b3363524218646ab0796e
Conga <alejandro.combasanz2004@gmail.com>
2023-08-30 00:17:09 -0300

    Correjido error de sintaxis

--- tests/TFranquiciaParcialTest.php
+++ tests/TFranquiciaParcialTest.php
@@ -5,12 +5,12 @@ namespace TrabajoSube;
 use PHPUnit\Framework\TestCase;
 
 class TFranquiciaParcialTest extends TestCase{
-    
+
     public function FParcialEsMitad(){
     
         $tarjeta = new TarjetaFranquiciaParcial();
         $colectivo = new Colectivo();
-        $this->assertEquals(($colectivo->boletoNormal * (100-$tarjeta2->porcentajeDescuento)/100), ($colectivo->boletoNormal / 2));
+        $this->assertTrue(($colectivo->boletoNormal * (100-$tarjeta->porcentajeDescuento)/100) == ($colectivo->boletoNormal / 2));
 
     }
 
