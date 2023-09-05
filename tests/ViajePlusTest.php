cd3e3c842d7b14375df7ef7e8e53609166abb441
martualarc <martina.alarcon029@gmail.com>
2023-09-05 00:15:26 -0300

    Test viaje plus

---
+++ tests/ViajePlusTest.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace TrabajoSube;
+
+use PHPUnit\Framework\TestCase;
+
+class ViajePlusTest extends TestCase{
+   
+    public function testDescuentaSaldo(){
+    $colectivo = new Colectivo();
+    $tarjeta = new Tarjeta();
+    $colectivo->pagarCon($tarjeta);
+    assertEquals(-($boletoNormal), $tarjeta->saldo);
+}
+
+}
