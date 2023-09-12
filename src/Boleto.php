47a41a0f40fe87b268a0193e1087d2c88397501a
martualarc <martina.alarcon029@gmail.com>
2023-09-12 16:48:24 -0300

    Implementada ID tarjeta y algunos metodos boleto

--- src/Boleto.php
+++ src/Boleto.php
@@ -3,16 +3,33 @@ namespace TrabajoSube;
 
 class Boleto{
     public $hora;
+    public $fecha;
     public $costoBoleto;
     public $saldoRestante;
+    public $idTarjeta;
 
-    function __construct($costoBoleto='Undefined', $saldoRestante='Undefined'){
+    function __construct($costoBoleto='Undefined', $saldoRestante='Undefined', $idTarjeta=0){
         $this->hora = date('h:i:s');
+        $this->fecha = date('d-m');
         $this->costoBoleto = $costoBoleto;
         $this->saldoRestante = $saldoRestante;
+        $this->$idTarjeta = $idTarjeta;
     }
 
-    function observar(){
-        echo ('Hora: ' . $this->hora . "\nCosto del Boleto: " . $this->costoBoleto . "\nSaldo Restante: " . $this->saldoRestante);
+    function getFecha(){
+        return $this->fecha;
     }
+
+    function getHora(){
+        return $this->hora;
+    }
+
+    function getCosto(){
+        return $this->costoBoleto;
+    }
+
+    function getSaldo(){
+        return $this->saldoRestante;
+    }
+    
 }
