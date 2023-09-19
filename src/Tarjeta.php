1f7b3f1040862f0ab8d6d336e1674a159fd8405d
Conga <alejandro.combasanz2004@gmail.com>
2023-09-18 23:01:03 -0300

    Añadida la funcionalidad del saldo pendiente

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -6,6 +6,7 @@ class Tarjeta{
     public $limite;
     public $limiteInferior;
     public $porcentajeDescuento;
+    public $saldoPendiente;
     
     
     function __construct(){
@@ -13,21 +14,37 @@ class Tarjeta{
         $this->limite = 6600; 
         $this->limiteInferior = -211.84;
         $this->porcentajeDescuento = 0;
+        $this->saldoPendiente = 0;
     }
+
+    function AcreditarYCalcularPendiente(){
+        if($this->saldo < $this->limite){
+            $this->saldo = $this->saldo + $this->pendiente;
+            $this->pendiente = 0;
+        }
+        if($this->saldo > $this->limite){
+            $this->pendiente = $this->pendiente + ($this->saldo - $this->limite);
+            $this->saldo = $this->limite;
+        }
+    }
+
 
     function cargar($monto){
         if($this->saldo + $monto <= $this->limite){
             if($monto >= 150){
                 if($monto <= 500 && (($monto % 50) == 0)){
                     $this->saldo = $this->saldo + $monto;
+                    AcreditarYCalcularPendiente();
                     return TRUE;
                 }
                 elseif($monto <= 1500 && (($monto % 100) == 0)){
                         $this->saldo = $this->saldo + $monto;
+                        AcreditarYCalcularPendiente();
                         return TRUE;
                     }
                 elseif($monto <= 4000 && (($monto % 500 == 0))){
                     $this->saldo = $this->saldo + $monto;
+                    AcreditarYCalcularPendiente();
                     return TRUE;
                 }
                 else{
