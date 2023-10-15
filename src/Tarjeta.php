6a99cb4255042b6acc024da5c9f9bb6c08690502
martualarc <martina.alarcon029@gmail.com>
2023-10-15 03:44:16 -0300

    Correcciones en saldo pendiente 2

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -28,21 +28,18 @@ class Tarjeta{
 
     function CalcularPendiente(){
         if($this->saldo > $this->limite){
-            $this->pendiente = $this->saldo - $this->limite;
-        }
-    }
-
-    function SaldoAlLimite(){
-        if($this->saldo > $this->limite){
-            $this->saldo = $this->limite;
+            $this->pendiente = $this->pendiente + ($this->saldo - $this->limite);
         }
     }
 
     function AcreditarPendiente(){
         if($this->saldo < $this->limite){
             $this->saldo = $this->saldo + $this->pendiente;
-            $this->CalcularPendiente();
         }
+        if($this->saldo > $this->limite){
+            $this->saldo = $this->limite;
+        }
+        $this->CalcularPendiente();
     }
 
 
@@ -51,19 +48,16 @@ class Tarjeta{
             if($monto <= 500 && (($monto % 50) == 0)){
                 $this->saldo = $this->saldo + $monto;
                 $this->CalcularPendiente();
-                $this->SaldoAlLimite();
                 return TRUE;
             }
             elseif($monto <= 1500 && (($monto % 100) == 0)){
                 $this->saldo = $this->saldo + $monto;
                     $this->CalcularPendiente();
-                    $this->SaldoAlLimite();
                     return TRUE;
                     }
             elseif($monto <= 4000 && (($monto % 500 == 0))){
                 $this->saldo = $this->saldo + $monto;
                 $this->CalcularPendiente();
-                $this->SaldoAlLimite();
                 return TRUE;
             }
             else{
