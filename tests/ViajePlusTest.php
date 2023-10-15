3ea53868b487ac669ffd492117c28b1088af87e2
martualarc <martina.alarcon029@gmail.com>
2023-10-14 21:48:27 -0300

    Correccion tiempo test (again)

--- tests/ViajePlusTest.php
+++ tests/ViajePlusTest.php
@@ -7,7 +7,8 @@ use PHPUnit\Framework\TestCase;
 class ViajePlusTest extends TestCase{
    
     public function testDescuentaSaldo(){
-    $colectivo = new Colectivo("Q", time(), date('d'));
+    $tiempo = new Tiempo();
+    $colectivo = new Colectivo("Q", $tiempo);
     $tarjeta = new Tarjeta();
     $colectivo->pagarCon($tarjeta);
     $this->assertEquals(-185, $tarjeta->saldo);
