030913c2b983ff36f5f9f11caedd2c14fff65cdb
martualarc <martina.alarcon029@gmail.com>
2023-10-15 02:41:57 -0300

    Test pendiente

--- tests/SaldoPendienteTest.php
+++ tests/SaldoPendienteTest.php
@@ -7,11 +7,25 @@ use PHPUnit\Framework\TestCase;
 class SaldoPendienteTest extends TestCase{
    
     public function testSaldoPendiente(){
-    $tarjeta = new Tarjeta;
-    $tarjeta->cargar(3500);
-    $tarjeta->cargar(3500);
-    $this->assertEquals(400, $tarjeta->pendiente);
+        $tarjeta = new Tarjeta;
+        $tarjeta->cargar(3500);
+        $tarjeta->cargar(3500);
 
-}
+        $this->assertEquals(6600, $tarjeta->saldo);
+        $this->assertEquals(400, $tarjeta->pendiente);
+    }
+
+    public function testRecargaPendiente(){
+        $tarjeta = new Tarjeta;
+        $tarjeta->cargar(3500);
+        $tarjeta->cargar(3280);
+
+        $tiempo = new Tiempo();
+        $colectivo = new Colectivo("138", $tiempo);
+
+        $boleto = $colectivo->pagarCon($tarjeta);
+        
+        $this->assertEquals(6600, $tarjeta->saldo);
+    }
 
 }
