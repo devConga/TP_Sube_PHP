a8ab68c7bd80d2a82bc182f8df9792dbf73f3905
martualarc <martina.alarcon029@gmail.com>
2023-10-16 01:07:08 -0300

    Test valor boleto interurbano

---
+++ tests/InterurbanoTest.php
@@ -0,0 +1,20 @@
+<?php 
+
+namespace TrabajoSube;
+
+use PHPUnit\Framework\TestCase;
+
+class InterurbanoTest extends TestCase{
+
+    public function testPrecioBoleto(){
+
+        $tiempo = new Tiempo();
+        $colectivo = new ColectivoInterurbano("35/9", $tiempo);
+        $tarjeta = new Tarjeta();
+        $tarjeta->cargar(200);
+
+        $boleto = $colectivo->pagarCon($tarjeta);
+
+        $this->assertEquals(16, $tarjeta->saldo);
+    }
+}
