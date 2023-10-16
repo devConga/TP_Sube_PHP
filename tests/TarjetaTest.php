b0689757c48ec120a128f065657740b3e379fba9
martualarc <martina.alarcon029@gmail.com>
2023-10-16 18:59:53 -0300

    Test descuento 80 viajes

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -36,4 +36,37 @@ class TarjetaTest extends TestCase{
 
     }
 
+    public function testUsoFrecuente80Viajes(){
+
+        $tiempo = new TiempoFalso();
+        $tarjeta = new Tarjeta();
+        $colectivo = new Colectivo("102R", $tiempo);
+
+        $i = 0;
+
+        $tarjeta->cargar(4000);
+        $tarjeta->cargar(2000);
+
+        while($i < 30){
+            $boleto = $colectivo->pagarCon($tarjeta);
+            $i += 1;
+        }
+
+        $tarjeta->cargar(4000);
+        $tarjeta->cargar(2000);
+
+        while($i < 70){
+            $boleto = $colectivo->pagarCon($tarjeta);
+            $i += 1;
+        }
+        $tarjeta->cargar(2000);
+        while($i < 81){
+            $boleto = $colectivo->pagarCon($tarjeta);
+            $i += 1;
+        }
+
+        $this->assertEquals(911.25, $tarjeta->saldo);
+
+    }
+
 }
