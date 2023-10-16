202884213f9c20dc6a5b646920aba0ceb1ad66e5
martualarc <martina.alarcon029@gmail.com>
2023-10-16 18:28:36 -0300

    Valor correcto de saldo en test

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -35,7 +35,7 @@ class TarjetaTest extends TestCase{
 
         $boleto = $colectivo->pagarCon($tarjeta);
 
-        $this->assertEquals(487, $tarjeta->saldo);
+        $this->assertEquals(302, $tarjeta->saldo);
         
 
     }
