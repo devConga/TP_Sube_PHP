0acc74285675b05a824023237ff957e895b3e815
martualarc <martina.alarcon029@gmail.com>
2023-10-16 16:38:45 -0300

    Test viaje fuera de franja

---
+++ tests/FranjaHorariaTest.php
@@ -0,0 +1,18 @@
+<?php 
+
+namespace TrabajoSube;
+
+use PHPUnit\Framework\TestCase;
+
+class FranjaHorariaTest extends TestCase{
+
+    public function testFranquiciaCompletaFranja(){
+        $tarjeta = new TFranquiciaCompleta();
+        $tiempoFalso = new TiempoFalso();
+        $colectivo = new Colectivo("115", $tiempoFalso);
+        $tarjeta->cargar(600);
+        $tiempoFalso->AvanzarSegundos(15000);
+
+        $this->assertEquals(415, $tarjeta->saldo);
+    }
+}
